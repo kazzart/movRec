27b6e27a5edb07fd5efe868d51779d1e6c5a8f46
Anastas Misailidi <misailidi.a@gmail.com>
2022-05-03 18:55:10 +0300

    Added tables

--- mov-rec-back/app/settings.py
+++ mov-rec-back/app/settings.py
@@ -4,9 +4,6 @@ class Settings(BaseSettings):
     development_mode: bool = True
     server_port: int
     database_url: str
-    secret: str
-    jwt_algorithm: str = 'HS256'
-    jwt_lifetime: int = 3600
 
 settings = Settings(
     _env_file='.env',
--- mov-rec-back/app/tables.py
+++ mov-rec-back/app/tables.py
@@ -1,11 +1,26 @@
-from sqlalchemy import Column, Integer, VARCHAR, Text
+from email.policy import default
+from sqlalchemy import Column, ForeignKey, Integer, VARCHAR, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    email = Column(VARCHAR(50), nullable=False, unique=True)
-    password = Column(Text, nullable=False)
+    nickname = Column(Text, default='default_nickname')
+
+
+class Movie(Base):
+    __tablename__ = 'movie'
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, nullable=False)
+    genres = Column(Text, nullable=False)
+
+
+class Rating(Base):
+    __tablename__ = 'rating'
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    movie_id = Column(Integer, ForeignKey('movie.id'), primary_key=True)
+    rating = Column(Integer, nullable=False)
